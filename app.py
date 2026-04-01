9b749ae54e6c570977c56363013c3d7d41e23657
pcoronel-sys <pcoronel@bago.com.ec>
2026-04-01 16:06:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -77,13 +77,13 @@ if st.session_state['pagina_actual'] == "inicio":
     
     with col_l:
         st.markdown(f'<p class="almacen-tag"></p>', unsafe_allow_html=True)
-        if st.button("\n\n Extra Ciclos"):
+        if st.button("\n\n EXTRA CICLOS"):
             st.session_state['pagina_actual'] = "sistema" # Dirige al sistema funcional
             st.rerun()
             
     with col_r:
         st.markdown(f'<p class="almacen-tag"></p>', unsafe_allow_html=True)
-        if st.button("\n\REPRO"):
+        if st.button("\n REPROGRAMA"):
             # NO CAMBIA DE PÁGINA - Se queda en el inicio
             st.toast("Módulo en desarrollo...", icon="⚠️")
             pass 
