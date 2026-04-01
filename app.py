1df5edeb1c788a87369b2f787ca9362e99949349
pcoronel-sys <pcoronel@bago.com.ec>
2026-04-01 16:03:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -73,10 +73,10 @@ if st.session_state['pagina_actual'] == "inicio":
     st.markdown('<p class="main-title">Laboratorios Bagó</p>', unsafe_allow_html=True)
     st.markdown("<h3 style='text-align:center; color:#555; font-weight:300; margin-bottom:60px;'>Intel-Stock Management</h3>", unsafe_allow_html=True)
     
-    _, col_l, col_r, _ = st.columns([2, 3, 3, 2])
+    _, col_l, col_r, _ = st.columns([6.5, 1.8, 1.8, 6.5])
     
     with col_l:
-        st.markdown(f'<p class="almacen-tag">extra</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="almacen-tag"></p>', unsafe_allow_html=True)
         if st.button("📦\n\n Extra Ciclos"):
             st.session_state['pagina_actual'] = "sistema" # Dirige al sistema funcional
             st.rerun()
