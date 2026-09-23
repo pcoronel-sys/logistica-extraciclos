27c59929766cd8752b491377c97e0eac476a9790
pcoronel-sys <pcoronel@bago.com.ec>
2026-09-23 16:31:12 -0500

    Change border-radius from 20px to 90px

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.markdown(f"""
         backdrop-filter: blur(15px) !important; 
         color: #333 !important; 
         border: 1px solid rgba(20, 20, 20, 0.3) !important; 
-        border-radius: 20px !important; 
+        border-radius: 90px !important; 
         height: 10px !important; 
         width: 100% !important; 
         box-shadow: 0 20px 40px rgba(0,0,0,0.05) !important; 
